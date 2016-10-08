199e3208a64ca4ec85ed798c5ec23907ee3e1207
Hiroshi Sawai <info@info-town.jp>
2016-10-08 10:51:47 +0900

    Add confirm dialog when row and column is removed on context menu.

--- src/TableEntry.js
+++ src/TableEntry.js
@@ -25,19 +25,20 @@ export default (function () {
         const data = $( '#it-table-editor-content' ).val();
         const content = $( current ).children( 'input[name="content[]"]' );
         if ( data !== '' ) {
-            content.parent().removeClass('it-table-data-empty');
-            content.parent().addClass('it-table-data-fill');
+            content.parent().removeClass( 'it-table-data-empty' );
+            content.parent().addClass( 'it-table-data-fill' );
             $( 'i', content.parent() ).removeClass( 'fa-pencil-square-o' );
             $( 'i', content.parent() ).addClass( 'fa-pencil-square' );
         }
         if ( data === '' ) {
-            content.parent().removeClass('it-table-data-fill');
-            content.parent().addClass('it-table-data-empty');
+            content.parent().removeClass( 'it-table-data-fill' );
+            content.parent().addClass( 'it-table-data-empty' );
             $( 'i', content.parent() ).removeClass( 'fa-pencil-square' );
             $( 'i', content.parent() ).addClass( 'fa-pencil-square-o' );
         }
         content.val( data );
         $( '#it-table-entry-dialog' ).hide();
+        content.parent().css( { 'background': 'transparent' } );
         return false;
     }
 
@@ -48,6 +49,7 @@ export default (function () {
      * @public
      */
     function edit() {
+        $( this ).css( { 'background': '#FFBF6D' } );
         current = this;
         const content = $( 'input[name="content[]"]', this );
         $( '#it-table-editor-content' ).val( content.val() );
